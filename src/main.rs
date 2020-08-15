592d08c6f39153d62188f25203fd35360f9c601f
AngelOfSol <julietckilian@gmail.com>
2020-08-15 12:22:17 -0400

    Reorg'd initial impl of engine interface.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,53 @@
-use ws_engine::engine::Engine;
+use std::io::*;
+use std::str::FromStr;
+use ws_engine::data::CardId;
+use ws_engine::engine::{io::IO, Engine};
+use ws_engine::state::phase::Phase;
+
+#[derive(Debug)]
+struct BasicIO;
+
+impl IO for BasicIO {
+    fn phase_change(&self, phase: Phase, turn_player: usize) {
+        println!("Phase Changed: {:?} for player {}", phase, turn_player);
+    }
+
+    fn draw(&self, turn_player: usize) {
+        println!("player {} drew a card", turn_player);
+    }
+
+    fn clock(&self, card: CardId, turn_player: usize) {
+        println!("player {} clocked card {}", turn_player, card);
+    }
+
+    fn ask_choice(&self, options: &Vec<CardId>, choosing_player: usize) -> Option<CardId> {
+        let mut choice_buffer = String::new();
+
+        println!(
+            "player {} may choose to clock from: {:?}",
+            choosing_player, options
+        );
+
+        let _ = stdout().flush();
+        let _ = stdin().read_line(&mut choice_buffer);
+
+        let choice_buffer = choice_buffer.trim();
+
+        if choice_buffer == "" {
+            None
+        } else {
+            let id = CardId::from_str(&choice_buffer).ok()?;
+            if options.contains(&id) {
+                Some(id)
+            } else {
+                None
+            }
+        }
+    }
+}
 
 fn main() {
-    let mut engine = Engine::new(());
+    let mut engine = Engine::new(BasicIO);
 
     for _ in 0..10 {
         engine.run_turn();
